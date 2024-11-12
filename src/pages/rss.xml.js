5ad625c08e11800f2015d6596eedf1841bfa862e
zqqcee <zqqcee@163.com>
2024-11-12 23:54:29 +0800

    feat: add rss feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,28 @@
+import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
+import dayjs from 'dayjs';
+
+export function GET(context) {
+	const postImportResult = import.meta.glob('./**/!(algorithm|febasic)/*.md', { eager: true });
+	const posts = Object.values(postImportResult);
+	return rss({
+		title: 'luckycc',
+		// `<description>` field in output xml
+		description: "zqqcee's blog page",
+		// Pull in your project "site" from the endpoint context
+		// https://docs.astro.build/en/reference/api-reference/#contextsite
+		site: context.site,
+		// Array of `<item>`s in output xml
+		// See "Generating items" section for examples using content collections and glob imports
+		items: posts.map((post) => ({
+			link: post.url,
+			content: sanitizeHtml(post.compiledContent()),
+			title: post.frontmatter.title || '无标题',
+			description: post.frontmatter.description || '',
+			pubDate: post.frontmatter.date || dayjs().format('YYYY-MM-DD'),
+		})),
+		// (optional) inject custom xml
+		customData: `<language>en-us</language>`,
+		stylesheet: '/rss/styles.xsl',
+	});
+}
